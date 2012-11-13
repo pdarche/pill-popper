cc001586cd8db5f3c01f4917ac669f5d23e56680
Peter Darche <pdarche@gmail.com>
2012-11-13 10:41:14 -0500

    pre-merge commit

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,49 +1,7 @@
 
 $( document ).ready(function(){
 
-	$.get('/check_meds', function(data){
-		var data = $.parseJSON(data)
-		console.log("data", data)
-		
-		var ts = Math.round((new Date()).getTime() / 1000) 
-			
-		$.each(data.user, function(i){
-			var classString = '.med.' + $.parseJSON(data.user[i]).medId,
-				interval = Number($(classString).find('.interval').html()) * 60 * 60			
-				userId = $.parseJSON(data.user[i]).userId,
-				userString = '.user-name.' + userId
-
-			if ( ts - $.parseJSON(data.user[i]).time_taken > interval ) {
-				$(userString).parent().parent().append('<div>Pills to Take</div>')
-			}
-			else {
-				$(classString).remove()
-				$(userString).parent().parent().append('<div>No Pills to Take!</div>')
-			} 
-
-			console.log($(userString).parent().parent())
-		})
-
-		$.each(data.affiliate, function(i){
-			var classString = '.med.' + $.parseJSON(data.affiliate[i]).medId,
-				interval = Number($(classString).find('.interval').html()) * 60 * 60			
-				userId = $.parseJSON(data.affiliate[i]).userId,
-				userString = '.user-name.' + userId
-
-			if ( ts - $.parseJSON(data.affiliate[i]).time_taken > interval ) {
-				$(userString).parent().parent().append('<div>Pills to Take</div>')
-			}
-			else {
-				$(classString).remove()
-				$(userString).parent().parent().append('<div>No Pills to Take!</div>')
-			} 
-
-			console.log($(userString).parent().parent())
-		})
-
-
-
-	})
+	checkMeds()
 
 	$('.content_wrapper').click(function(){
 		$(this).find('.medications').fadeIn()
@@ -81,6 +39,8 @@ $( document ).ready(function(){
 	// Set drag scroll on first descendant of class dragger on both selected elements
 	$('div.viewport').dragscrollable({dragSelector: '.bottle:first', acceptPropagatedEvent: false});
 
+	
+
 })
 
 function getClass(classes, index){
@@ -88,5 +48,46 @@ function getClass(classes, index){
     return split[index];
  }
 	 
+function checkMeds(){
+	$.get('/check_meds', function(data){
+		var data = $.parseJSON(data)
+		console.log("data", data)
+		
+		var ts = Math.round((new Date()).getTime() / 1000) 
+			
+		$.each(data.user, function(i){
+			var classString = '.med.' + $.parseJSON(data.user[i]).medId,
+				interval = Number($(classString).find('.interval').html()) * 60 * 60			
+				userId = $.parseJSON(data.user[i]).userId,
+				userString = '.user-name.' + userId
 
+			if ( ts - $.parseJSON(data.user[i]).time_taken > interval ) {
+				$('.user').eq(0).parent().append('<div>Pills to Take</div>')
+			}
+			else {
+				$(classString).remove()
+				$('.user').eq(0).parent().append('<div>No Pills to Take!</div>')
+			} 
+
+			console.log($(userString).parent().parent())
+		})
+
+		$.each(data.affiliate, function(i){
+			var classString = '.med.' + $.parseJSON(data.affiliate[i]).medId,
+				interval = Number($(classString).find('.interval').html()) * 60 * 60			
+				userId = $.parseJSON(data.affiliate[i]).userId,
+				userString = '.user-name.' + userId
+
+			if ( ts - $.parseJSON(data.affiliate[i]).time_taken > interval ) {
+				$('.affiliate').parent().append('<div>Pills to Take</div>')
+			}
+			else {
+				$(classString).remove()
+				$('.affiliate').parent().append('<div>No Pills to Take!</div>')
+			} 
+
+			console.log($(userString).parent())
+		})
+	})
+}
 
